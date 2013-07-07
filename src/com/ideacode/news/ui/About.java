46eadc9f9a8a8a9aba283c2e73f1997bbf99182b
andyliu900 <andyliu900@gmail.com>
2013-07-07 13:54:33 +0800

    测试提交

--- src/com/ideacode/news/ui/About.java
+++ src/com/ideacode/news/ui/About.java
@@ -10,30 +10,30 @@ import com.ideacode.news.logic.IdeaCodeActivity;
 
 public class About extends IdeaCodeActivity {
 
-    private View view;
-    private TextView headTv;
+	private View view;
+	private TextView headTv;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        view = this.getLayoutInflater().inflate(R.layout.activity_about, null);
-        setContentView(view);
-        
-        headTv = (TextView) findViewById(R.id.systv);
-        headTv.setText(R.string.about_head_title);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		view = this.getLayoutInflater().inflate(R.layout.activity_about, null);
+		setContentView(view);
 
-    @Override
-    public void init() {
-        // TODO Auto-generated method stub
+		headTv = (TextView) findViewById(R.id.systv);
+		headTv.setText(R.string.about_head_title);
+	}
 
-    }
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
 
-    @Override
-    public void refresh(Object... param) {
-        // TODO Auto-generated method stub
+	}
 
-    }
+	@Override
+	public void refresh(Object... param) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
